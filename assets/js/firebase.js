35e65b367f49a5952b9e32d6943d0a3eb65f2393
Mike <grossmi4@gmail.com>
2018-09-25 19:58:02 -0400

    not sure what I changed

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -8,34 +8,42 @@ var config = {
   messagingSenderId: "757199431159"
 };
 firebase.initializeApp(config);
-database = firebase.database();
-employeeRef = database.ref('Employees/');
+database = firebase.database(); //set primary database object
+employeeRef = database.ref('Employees/'); //primary database reference for lists
 
+//set action for when submit is clicked to push form information into firebase as a new record
 $("#submit-btn").on("click",function(event){
   event.preventDefault();
-  //set local variables to input values for validation
+  //set form inputs to local variables
   const employeeName = $("#employee-name").val().trim();
   const role = $("#role").val().trim();
   const startDate = $("#start-date").val().trim();
   const monthlyRate = $("#monthly-rate").val().trim();
+  const dateAdded = moment();
 
+  //creates single object with all the local variables above
   const postData = {
     employeeName,
     role,
     startDate,
     monthlyRate,
+    dateAdded,
   };
   console.log(postData);
 
-  employeeRef.push(postData)
+  employeeRef.push(postData) //creates row in database
 
 });
 
+
+//action that runs when an item is added to the list in firebase
 employeeRef.on('child_added', function(snapshot){
   console.log(snapshot.val());
   const data = snapshot.val();
   const monthsWorked = 2; //need to use moment.j
   const totalBilled = 20000;
+
+  //create new row using string literals
   const newRow = `
   <th>${data.employeeName}</th>
   <th>${data.role}</th>
@@ -44,6 +52,7 @@ employeeRef.on('child_added', function(snapshot){
   <th>${data.monthlyRate}</th>
   <th>${totalBilled}</th>
   `;
-  $("#employeeTable").append(newRow)
+
+  $("#employeeTable").append(newRow); //adds row to table on page
 
 });
