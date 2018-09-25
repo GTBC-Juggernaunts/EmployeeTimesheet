4b84517ed69d28fa63d74d1d4d52c79e9b9c85f3
Mike <grossmi4@gmail.com>
2018-09-25 19:34:18 -0400

    added submit and on child add (alpha)

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -8,3 +8,40 @@ var config = {
   messagingSenderId: "757199431159"
 };
 firebase.initializeApp(config);
+database = firebase.database();
+employeeRef = firebase.database('Employees/');
+
+$("#submit-btn").on("click",function(event){
+  event.preventDefault();
+  //set local variables to input values for validation
+  const employeeName = $("#employee-name").val().trim();
+  const role = $("#role").val().trim();
+  const startDate = $("#start-date").val().trim();
+  const monthlyRate = $("#monthly-rate").val().trim();
+
+  const postData = {
+    employeeName,
+    role,
+    startDate,
+    monthlyRate,
+  };
+
+  employeeRef.push(postData)
+
+};
+
+employeeRef.on('child_added', function(snapshot){
+  console.log(snapshot.val());
+  const monthsWorked = 2; //need to use moment.j
+  const totalBilled = 20000;
+  const newRow = `
+  <th>${employeeName}</th>
+  <th>${role}</th>
+  <th>${startDate}</th>
+  <th>${monthsWorked}</th>
+  <th>${monthlyRate}</th>
+  <th>${totalBilled}</th>
+  `;
+  $("#employeeTable").append(newRow)
+
+});
