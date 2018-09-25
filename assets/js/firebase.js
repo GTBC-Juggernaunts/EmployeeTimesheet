2bc64b36fb3af76e205a9b7d7b93815c8345bdd9
Darron Biles <darron.biles@gmail.com>
2018-09-25 19:20:12 -0400

    Update: Firebase and js files moved

---
+++ assets/js/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAnYhahnSp6y-YWhWwyRp4XyqiQWvzR0_E",
+  authDomain: "timesheet-cc996.firebaseapp.com",
+  databaseURL: "https://timesheet-cc996.firebaseio.com",
+  projectId: "timesheet-cc996",
+  storageBucket: "",
+  messagingSenderId: "757199431159"
+};
+firebase.initializeApp(config);
